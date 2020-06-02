b4a226b9f1490eed2588761a7ecf08802c36328d
liamhartley <liamjhartley@yahoo.co.uk>
2020-06-02 12:20:42 +0100

    updating website during job move

--- random_landscape.js
+++ random_landscape.js
@@ -4,5 +4,5 @@ var imgCount = 3;
     var images = new Array
             images[1] = "eastonriver_l.jpeg",
             images[2] = "towerbridge_l.jpeg",
-            images[3] = "victoriapark_l.jpeg",
+            images[3] = "stormybridge_l.jpeg",
     document.getElementById("landscapeID").style.backgroundImage = "url(" + dir + images[randomCount] + ")";
--- random_picture.js
+++
@@ -1,8 +0,0 @@
-var imgCount = 3;
-    var dir = 'images/';
-    var randomCount = Math.round(Math.random() * (imgCount - 1)) + 1;
-    var images = new Array
-            images[1] = "pic1.jpeg",
-            images[2] = "pic2.jpg",
-            images[3] = "pic3.jpeg",
-    document.getElementById("divID").style.backgroundImage = "url(" + dir + images[randomCount] + ")";
