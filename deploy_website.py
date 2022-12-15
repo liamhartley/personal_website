2c9c554f77cc7dbbf7d2b42ff07dcb4e7a59b0a1
liamhartley <liamjhartley@yahoo.co.uk>
2022-12-15 14:21:32 +0000

    refactoring reports

--- deploy_website.py
+++ deploy_website.py
@@ -3,11 +3,11 @@ import os
 
 if __name__ == '__main__':
     files_to_upload = [
-        'about.html',
+        # 'about.html',
         # 'hireme.html',
         # 'index.html',
         # 'bettingguide.html',
-        # 'football.html',
+        'football.html',
         # 'tiktok.html'
     ]
 
